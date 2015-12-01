fc64a357dba19d1f90c5055d7f70f88de8bde34b
catris25 <blue.star95@outlook.com>
2015-12-01 22:18:41 +0700

    First Commit

---
+++ src/lostandfound/BarangController.java
@@ -0,0 +1,23 @@
+package lostandfound;
+
+/**
+ *
+ * @author LIA
+ */
+public class BarangController {
+    public void tambahBarang(){
+        
+    }
+    
+    public void ambilBarang(){
+        
+    }
+    
+    public void lihatBarangHilang(){
+        
+    }
+    
+    public void lihatSemuaBarang(){
+        
+    }
+}
---
+++ src/lostandfound/LAFMain.java
@@ -0,0 +1,14 @@
+package lostandfound;
+
+/**
+ *
+ * @author LIA
+ */
+public class LAFMain {
+
+    public static void main(String[] args) {
+        LAFMainMenu runMenu = new LAFMainMenu();
+        runMenu.main(args);
+    }
+    
+}
